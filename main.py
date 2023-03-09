ad7febc369972daa6f24c401f9de6ba7bc96bbe5
Artem Obukhov <arteam_101@mail.ru>
2023-03-09 16:31:42 +0700

    Начал делать бота

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from twitchio.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# set up the bot
+bot = commands.Bot(
+    token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+
+@bot.event
+async def event_message(ctx):
+    print(ctx.author.name)
+
+
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.send('test passed!')
+    
+
+if __name__ == '__main__':
+    bot.run()

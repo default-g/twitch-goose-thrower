cd7b872ccfa5d5efd10a0ad479e0135571cec7d7
Artem Obukhov <arteam_101@mail.ru>
2023-07-30 19:02:38 +0700

    Рефакторинг

--- main.py
+++ main.py
@@ -5,41 +5,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# set up the bot
-bot = commands.Bot(
-    token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
-    prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
-)
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(
+            token=os.environ['TMI_TOKEN'],
+            client_id=os.environ['CLIENT_ID'],
+            nick=os.environ['BOT_NICK'],
+            prefix=os.environ['BOT_PREFIX'],
+            initial_channels=[os.environ['CHANNEL']]
+        )
 
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+    async def event_ready(self):
+        print(f"{os.environ['BOT_NICK']} is online!")
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
 
 
-@bot.event
-async def event_message(ctx):
-    print(ctx.author.name)
+    @commands.command(name='test')
+    async def test(self, ctx):
+        await ctx.send('test passed!')
 
 
-
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send('test passed!')
-
-
-@bot.command(name='goose')
-async def goose(ctx):
-    f = open('messages/goose.txt', 'r')
-    for line in f.readlines():
-        await ctx.send(line)
+    @commands.command(name='goose')
+    async def goose(self, ctx):
+        f = open('messages/goose.txt', 'r')
+        for line in f.readlines():
+            await ctx.send(line)
 
 
-if __name__ == '__main__':
-    bot.run()
+bot = Bot()
+bot.run()
